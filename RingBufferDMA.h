0bb01b4e6d5b8e1dc33deb24a8b8377eb009ec63
pedvide <pedvide@gmail.com>
2017-11-26 23:09:58 +0100

    first working version

--- RingBufferDMA.h
+++ RingBufferDMA.h
@@ -26,79 +26,104 @@
 #ifndef RINGBUFFERDMA_H
 #define RINGBUFFERDMA_H
 
-#include <Arduino.h> // for digitalWrite
 #include "DMAChannel.h"
+#include "ADC_Module.h"
 
 
 /** Class RingBufferDMA implements a DMA ping-pong buffer of fixed size
 */
+template<uint32_t max_capacity>
 class RingBufferDMA
 {
     public:
-        //! Constructor, buffer has a size len and stores the conversions of ADC number ADC_num
-        RingBufferDMA(volatile int16_t* elems, uint32_t len, uint8_t ADC_num = 0);
+        //! Constructor, buffer stores the conversions of ADC number ADC_num
+        /** There are no interrupts when the buffer is full.
+        */
+        RingBufferDMA(volatile int16_t* const elems, ADC_NUM ADC_num) : RingBufferDMA(elems, ADC_num, &do_nothing_isr) {
+            dmaChannel.clearInterrupt();
+            dmaChannel.detachInterrupt();
+        }
+        //! Constructor, buffer stores the conversions of ADC number ADC_num
+        /**
+        *   \param dma_isr is a function pointer than will be called when the buffer is full.
+        */
+        RingBufferDMA(volatile int16_t* const elems, ADC_NUM ADC_num, void (*dma_isr)(void)) :
+            ADC_RA(ADC_num==ADC_NUM::ADC_0 ? ADC0_RA : ADC1_RA),
+            elems(elems)
+        {
+            static_assert(max_capacity%2==0, "RingBufferDMA's max_capacity must be a power of two.");
+
+            // set up a DMA channel to store the ADC data
+            // The idea is to have ADC_RA as a source,
+            // each ADC conversion triggers a DMA transfer of size 2 bytes
+            // when it's full it will overwrite the old elements.
+            dmaChannel.source(ADC_RA);
+            dmaChannel.destinationBuffer(elems, 2*max_capacity); // 2 bytes per element
+            dmaChannel.transferSize(2); // both SRC and DST size
+            dmaChannel.interruptAtCompletion(); //interruptAtHalf or interruptAtCompletion
+
+            uint8_t DMAMUX_SOURCE_ADC = DMAMUX_SOURCE_ADC0;
+            #if ADC_NUM_ADCS>1
+            if(ADC_num==ADC_NUM::ADC_1){
+                DMAMUX_SOURCE_ADC = DMAMUX_SOURCE_ADC1;
+            }
+            #endif // ADC_NUM_ADCS
+
+            dmaChannel.triggerAtHardwareEvent(DMAMUX_SOURCE_ADC); // start DMA transfer when ADC finishes a conversion
+            dmaChannel.attachInterrupt(dma_isr);
+        }
 
         //! Destructor
-        ~RingBufferDMA();
+        ~RingBufferDMA() {
+            dmaChannel.detachInterrupt();
+            dmaChannel.disable();
+        }
 
         //! Returns true if the buffer is full
-        bool isFull();
+        bool isFull() {
+            return dmaChannel.complete();
+        }
 
         //! Returns true if the buffer is empty
-        bool isEmpty();
-
-        //! Read a value from the buffer, make sure it's not emtpy by calling isEmpty() first
-        int16_t read();
+        bool isEmpty() {
+            return false;
+        }
 
         //! Start DMA operation
-        void start(void (*call_dma_isr)(void));
+        void start() {
+            // read ADC_RA to clear any DMA trigger
+            volatile uint16_t val __attribute__((unused)) = ADC_RA;
+            dmaChannel.enable();
+        }
 
-        //! Write a value into the buffer
-        /** The actual value is copied by DMA, this function only updates the buffer pointers to reflect that fact.
-        */
-        void write();
-
-        //! Length of the buffer
-        uint16_t size() {return b_size; }
+        //! Position DMA will write to next
+        uint32_t write_pos() {
+            //return (reinterpret_cast<uint32_t>(dmaChannel.destinationAddress()) - reinterpret_cast<uint32_t>(elems))/2;
+            return (int16_t*)dmaChannel.destinationAddress() - elems;
+        }
 
         //! Pointer to the data
-        volatile int16_t* const buffer() {return p_elems;}
+        volatile int16_t* const buffer() {return elems;}
 
         //! DMAChannel to handle all low level DMA code.
-        DMAChannel* dmaChannel;
+        DMAChannel dmaChannel{};
 
-
-        // the buffer needs to be aligned, so use malloc instead of new
-        // see http://stackoverflow.com/questions/227897/solve-the-memory-alignment-in-c-interview-question-that-stumped-me/
-        //uint8_t alignment;
-        //void *p_mem;
-
-        //! Start pointer: Read here
-        uint16_t b_start;
-        //! End pointer: Write here
-        uint16_t b_end;
-
-        //! Pointer to the elements of the buffer
-        volatile int16_t* const p_elems;
 
     protected:
     private:
 
-
-
         //! Size of buffer
-        uint16_t b_size;
+        const uint32_t b_size = max_capacity;
 
         //! ADC module of the instance
         uint8_t ADC_number;
 
-        //! Increases the pointer modulo 2*size-1
-        uint16_t increase(uint16_t p);
+        volatile uint32_t& ADC_RA;
 
-        volatile uint32_t* const ADC_RA;
+        //! Pointer to the elements of the buffer
+        volatile int16_t* const elems;
 
-
-
+        static void do_nothing_isr() {}
 
 };
 
--- examples/ringBufferDMA/ringBufferDMA.ino
+++ examples/ringBufferDMA/ringBufferDMA.ino
@@ -12,21 +12,13 @@ ADC *adc = new ADC(); // adc object
 
 // Define the array that holds the conversions here.
 // buffer_size must be a power of two.
-// The buffer is stored with the correct alignment in the DMAMEM section
-// the +0 in the aligned attribute is necessary b/c of a bug in gcc.
 const uint8_t buffer_size = 8;
-DMAMEM static volatile int16_t __attribute__((aligned(buffer_size+0))) buffer[buffer_size];
+
+volatile int16_t buffer[buffer_size];
 
 // use dma with ADC0
-RingBufferDMA *dmaBuffer = new RingBufferDMA(buffer, buffer_size, ADC_NUM::ADC_0);
+RingBufferDMA<buffer_size> dmaBuffer(buffer, ADC_NUM::ADC_0);
 
-#if ADC_NUM_ADCS>1
-//const int buffer_size2 = 8;
-//DMAMEM static volatile int16_t __attribute__((aligned(buffer_size2+0))) buffer2[buffer_size2];
-//
-//// use dma with ADC1
-//RingBufferDMA *dmaBuffer2 = new RingBufferDMA(buffer2, buffer_size2, ADC_NUM::ADC_1);
-#endif // defined
 
 void setup() {
 
@@ -35,51 +27,50 @@ void setup() {
 
     Serial.begin(9600);
 
-    // reference can be ADC_REFERENCE::REF_3V3, ADC_REFERENCE::REF_1V2 (not for Teensy LC) or ADC_REF_EXT.
-    //adc->setReference(ADC_REFERENCE::REF_1V2, ADC_NUM::ADC_0); // change all 3.3 to 1.2 if you change the reference to 1V2
-
     adc->setAveraging(ADC_AVERAGES::_8); // set number of averages
     adc->setResolution(ADC_RESOLUTION::_12); // set bits of resolution
 
-
-    // always call the compare functions after changing the resolution!
-    //adc->enableCompare(1.0/3.3*adc->getMaxValue(ADC_NUM::ADC_0), ADC_CMP::OK_IF_SMALLER, ADC_NUM::ADC_0); // measurement will be ready if value < 1.0V
-    //adc->enableCompareRange(1.0*adc->getMaxValue(ADC_NUM::ADC_1)/3.3, 2.0*adc->getMaxValue(ADC_NUM::ADC_1)/3.3, 0, 1, ADC_NUM::ADC_1); // ready if value lies out of [1.0,2.0] V
-
     // enable DMA and interrupts
-    adc->enableDMA(ADC_NUM::ADC_0);
+    adc->enableDMA();
 
     // ADC interrupt enabled isn't mandatory for DMA to work.
-    adc->enableInterrupts(ADC_NUM::ADC_0);
+    //adc->enableInterrupts(ADC_NUM::ADC_0);
+
+    //dmaBuffer.start();
+    dmaBuffer.start();
+
+
+    adc->analogRead(readPin, ADC_NUM::ADC_0);
+    adc->adc0->stopPDB();
+    // At 2 Hz it's easy to see the buffer being filled.
+    adc->adc0->startPDB(2);
 }
 
 char c=0;
 
 
+
 void loop() {
 
-     if (Serial.available()) {
-      c = Serial.read();
-      if(c=='s') { // start dma
-            Serial.println("Start DMA");
-            dmaBuffer->start(&dmaBuffer_isr);
-      } else if(c=='c') { // start conversion
-          Serial.println("Conversion: ");
-          adc->analogRead(readPin, ADC_NUM::ADC_0);
-      } else if(c=='p') { // print buffer
-          printBuffer();
-      } else if(c=='l') { // toggle led
-          digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
-      } else if(c=='r') { // read
-          Serial.print("read(): ");
-          Serial.println(dmaBuffer->read());
-      } else if(c=='f') { // full?
-          Serial.print("isFull(): ");
-          Serial.println(dmaBuffer->isFull());
-      } else if(c=='e') { // empty?
-          Serial.print("isEmpty(): ");
-          Serial.println(dmaBuffer->isEmpty());
-      }
+    if (Serial.available()) {
+        c = Serial.read();
+        if(c=='c') { // start conversion
+            Serial.println("Conversion.");
+            adc->analogRead(readPin, ADC_NUM::ADC_0);
+        } else if(c=='p') { // print buffer
+            printBuffer();
+        } else if(c=='l') { // toggle led
+            digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
+        } else if(c=='r') { // read
+            //Serial.print("read(): ");
+            //Serial.println(dmaBuffer->read());
+        } else if(c=='f') { // full?
+            Serial.print("isFull(): ");
+            Serial.println(dmaBuffer.isFull());
+        } else if(c=='e') { // empty?
+            Serial.print("isEmpty(): ");
+            Serial.println(dmaBuffer.isEmpty());
+        }
   }
 
 
@@ -91,7 +82,7 @@ void dmaBuffer_isr() {
     //digitalWriteFast(LED_BUILTIN, !digitalReadFast(LED_BUILTIN));
     Serial.println("dmaBuffer_isr");
     // update the internal buffer positions
-    dmaBuffer->dmaChannel->clearInterrupt();
+    dmaBuffer.dmaChannel.clearInterrupt();
 }
 
 
@@ -108,8 +99,8 @@ void printBuffer() {
 
     uint8_t i = 0;
     // we can get this info from the dmaBuffer object, even though we should have it already
-    volatile int16_t* buffer = dmaBuffer->buffer();
-    for (i = 0; i < dmaBuffer->size(); i++) {
+    volatile int16_t* buffer = dmaBuffer.buffer();
+    for (i = 0; i < buffer_size; i++) {
         Serial.print(uint32_t(&buffer[i]), HEX);
         Serial.print(", ");
         Serial.println(buffer[i]);
